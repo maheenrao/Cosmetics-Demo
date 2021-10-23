0531adb60e6f7fcfd0896c6cb39af9405d2a9bac
maheenrao <66237832+maheenrao@users.noreply.github.com>
2021-10-23 23:33:58 +0500

    edited

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import Products from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./Store/CartProvider";
 
-
 function App() {
- const [cartIsShown, setCartIsShown] = useState(false)
-  const showCartHandler =()=>{
-    setCartIsShown(true)
-    console.log(showCartHandler)
-  }
-  const hideCartHandler =()=>{
-    setCartIsShown(false)
-  }
- return (
-    
-    <CartProvider> 
-    {cartIsShown && <Cart onClose={hideCartHandler} />}
-    <Header onShowCart={showCartHandler} />
-      
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+    console.log(showCartHandler);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+
       <main>
-     <Products/>
-     </main>
+        <Products />
+      </main>
     </CartProvider>
   );
 }
